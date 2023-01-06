5181d7877405954b37b0a2069601ce62e1750baa
klauSigma <113836234+klauSigma@users.noreply.github.com>
2023-01-06 18:25:33 +0200

    Mostly Postcard UI

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using BlazorApp2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
